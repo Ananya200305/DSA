c8126383a316efacfc00f90119468ed41e75d25c
ananya03 <aggarwalananya03@gmail.com>
2025-01-23 13:55:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (51.93%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    // Helper function to determine if the array can be split into `k` subarrays
-    // with each subarray's sum <= maxSum
-    bool canSplit(vector<int>& nums, int k, int maxSum) {
-        int currentSum = 0, subarrays = 1;
-        for (int num : nums) {
-            if (currentSum + num > maxSum) {
-                currentSum = num; // Start a new subarray
-                subarrays++;
-                if (subarrays > k) return false; // Too many subarrays
-            } else {
-                currentSum += num;
+    bool possible(vector<int>& nums, int k, int maxSum){
+        int currSum = 0;
+        int subarray = 1;
+        for(int i = 0; i< nums.size(); i++){
+            if(currSum + nums[i] > maxSum){
+                currSum = nums[i];
+                subarray++;
+                if(subarray > k){
+                    return false;
+                }
+            }else{
+                currSum += nums[i];
             }
+            
         }
         return true;
-    }
-    
+    };
     int splitArray(vector<int>& nums, int k) {
-        // Binary search bounds
         int low = *max_element(nums.begin(), nums.end());
         int high = accumulate(nums.begin(), nums.end(), 0);
         int result = high;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (canSplit(nums, k, mid)) {
-                result = mid; // Possible candidate, try for smaller largest sum
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(possible(nums,k,mid)){
+                result = mid;
                 high = mid - 1;
-            } else {
-                low = mid + 1; // Increase the largest sum
+            }else{
+                low = mid + 1;
             }
         }
         return result;
