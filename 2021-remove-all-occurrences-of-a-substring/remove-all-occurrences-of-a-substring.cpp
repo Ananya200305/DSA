dd179d8174da68738449fcc1e8318ec46c70a21a
ananya03 <aggarwalananya03@gmail.com>
2025-02-11 08:43:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (90.62%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-        string result = "";
-        stack<char> st;
-        int i = 0;
-        for(int i = 0; i < s.length(); i++){
-            st.push(s[i]);
+        // string result = "";
+        // stack<char> st;
+        // int i = 0;
+        // for(int i = 0; i < s.length(); i++){
+        //     st.push(s[i]);
             
-            if(st.size() >= part.size()){
-                string temp = "";
-                stack<char> t;
-                for(int j = 0 ; j < part.size(); j++){
-                    t.push(st.top());
-                    temp = st.top() + temp;
-                    st.pop();
-                }
+        //     if(st.size() >= part.size()){
+        //         string temp = "";
+        //         stack<char> t;
+        //         for(int j = 0 ; j < part.size(); j++){
+        //             t.push(st.top());
+        //             temp = st.top() + temp;
+        //             st.pop();
+        //         }
 
-                if(temp != part){
-                    while(!t.empty()){
-                        st.push(t.top());
-                        t.pop();
-                    }
-                }
-            }
+        //         if(temp != part){
+        //             while(!t.empty()){
+        //                 st.push(t.top());
+        //                 t.pop();
+        //             }
+        //         }
+        //     }
+        // }
+        // while(!st.empty()){
+        //     result = st.top() + result;
+        //     st.pop();
+        // }
+        // return result;
+
+        while(s.length() > 0 && s.length() > s.find(part)){
+            s.erase(s.find(part), part.length());
         }
-        while(!st.empty()){
-            result = st.top() + result;
-            st.pop();
-        }
-        return result;
+        return s;
     }
 };
