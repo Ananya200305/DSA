f9b459a95c00af1ea4947fcc7224283f2c3c15da
ananya03 <aggarwalananya03@gmail.com>
2025-02-14 21:58:20 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (7.61%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,37 +1,62 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int row = matrix.size();
-        int col = matrix[0].size();
+
+        // Brute Force
+
+        // int row = matrix.size();
+        // int col = matrix[0].size();
         
-        int size = row*col;
-        vector<int> arr(size);
+        // int size = row*col;
+        // vector<int> arr(size);
 
-        int i = 0;
-        int j = 0;
-        int k = 0;
+        // int i = 0;
+        // int j = 0;
+        // int k = 0;
 
-        while(i < row){
-            int j = 0;
-            while(j < col){
-                arr[k] = matrix[i][j];
-                k++;
-                j++;
-            }
-            i++;
-        }
+        // while(i < row){
+        //     int j = 0;
+        //     while(j < col){
+        //         arr[k] = matrix[i][j];
+        //         k++;
+        //         j++;
+        //     }
+        //     i++;
+        // }
+
+        // int low = 0;
+        // int high = arr.size() - 1;
+
+        // while(low <= high){
+        //     int mid = low + (high - low)/2;
+        //     if(target == arr[mid]){
+        //         return true;
+        //     }else if(target < arr[mid]){
+        //         high = mid - 1;
+        //     }else{
+        //         low = mid + 1;
+        //     }
+        // }
+        // return false;
+
+        // Binary Search
+
+        int n = matrix.size();
+        int m = matrix[0].size();
 
         int low = 0;
-        int high = arr.size() - 1;
+        int high = m*n-1;
 
         while(low <= high){
-            int mid = low + (high - low)/2;
-            if(target == arr[mid]){
+            int mid = low + (high-low)/2;
+            int midTarget = matrix[mid/m][mid%m];
+
+            if(midTarget == target){
                 return true;
-            }else if(target < arr[mid]){
-                high = mid - 1;
+            }else if(midTarget < target){
+                low = mid+1;
             }else{
-                low = mid + 1;
+                high = mid - 1;
             }
         }
         return false;
