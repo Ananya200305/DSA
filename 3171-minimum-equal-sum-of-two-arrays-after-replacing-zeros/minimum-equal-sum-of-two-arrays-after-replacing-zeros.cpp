be1ca3e8b175d795ef5fdd7efee99ac5b3334835
ananya03 <aggarwalananya03@gmail.com>
2025-05-10 12:01:15 +0530

    Time: 132 ms (52.20%) | Memory: 135.6 MB (15.59%) - LeetSync

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -4,13 +4,13 @@ public:
         long long sum1 = 0;
         long long sum2 = 0;
 
-        bool zero1 = 0;
-        bool zero2 = 0;
+        bool zero1 = false;
+        bool zero2 = false;
 
         for(int i = 0; i < nums1.size(); i++){
             sum1 = sum1 + nums1[i];
             if(nums1[i] == 0){
-                zero1 = 1;
+                zero1 = true;
                 sum1++;
             }
         }
@@ -18,15 +18,16 @@ public:
         for(int i = 0; i < nums2.size(); i++){
             sum2 = sum2 + nums2[i];
             if(nums2[i] == 0){
-                zero2 = 1;
+                zero2 = true;
                 sum2++;
             }
         }
 
-        if(sum1 > sum2 && zero2 == 0){
+        if(sum1 > sum2 && zero2 == false){
             return -1;
         }
-        if(sum2 > sum1 && zero1 == 0){
+
+        if(sum1 < sum2 && zero1 == false){
             return -1;
         }
 
