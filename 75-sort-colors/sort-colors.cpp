db6b37eac5ff5dbf244167622a61baef23fdb5e5
ananya03 <aggarwalananya03@gmail.com>
2025-05-17 13:32:51 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (97.40%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // const char red = 0;
-        // const char white = 1;
-        // const char blue = 2;
+        const char red = 0;
+        const char white = 1;
+        const char blue = 2;
 
-        // int l = 0;
-        // int m = 0;
-        // int r = nums.size() - 1;
+        int l = 0;
+        int m = 0;
+        int r = nums.size() - 1;
 
-        // while(m <= r){
-        //     switch(nums[m]){
-        //         case red:
-        //             swap(nums[l],nums[m]);
-        //             l++;
-        //             m++;
-        //             break;
-        //         case white :
-        //             m++;
-        //             break;
-        //         case blue :
-        //             swap(nums[m], nums[r]);
-        //             r--;
-        //             break;
-        //     }
-        // }
-        sort(nums.begin(), nums.end());
+        while(m <= r){
+            switch(nums[m]){
+                case red:
+                    swap(nums[l],nums[m]);
+                    l++;
+                    m++;
+                    break;
+                case white :
+                    m++;
+                    break;
+                case blue :
+                    swap(nums[m], nums[r]);
+                    r--;
+                    break;
+            }
+        }
     }
 };
