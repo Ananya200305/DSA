eb97cc800af3de6f682442c81294e1289f1254fa
ananya03 <aggarwalananya03@gmail.com>
2025-05-24 10:05:24 +0530

    Time: 7 ms (5.91%) | Memory: 33.8 MB (12.10%) - LeetSync

---
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> res;
+
+        for(int i = 0; i < words.size(); i++){
+            string word = words[i];
+            for(int j = 0;  j < word.size(); j++){
+                if(word[j] == x){
+                    res.push_back(i);
+                    break;
+                }
+            }
+            continue;
+        }
+
+        return res;
+    }
+};
