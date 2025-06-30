be2e4112bbf9e2f80241cf36156f5a664ccf5944
ananya03 <aggarwalananya03@gmail.com>
2025-06-30 08:54:26 +0530

    Time: 21 ms (56.56%) | Memory: 43.8 MB (47.36%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
@@ -1,18 +1,40 @@
 class Solution {
 public:
     int findLHS(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
 
-        int left = 0; 
+        // O(n log n)
+
+        // sort(nums.begin(), nums.end());
+
+        // int left = 0; 
+        // int maxLen = 0;
+
+        // for(int right = 0; right < nums.size(); right++){
+        //     while(nums[right] - nums[left] > 1){
+        //         ++left;
+        //     }
+
+        //     if(nums[right] - nums[left] == 1){
+        //         maxLen = max(maxLen , right - left + 1);
+        //     }
+        // }
+
+        // return maxLen;
+
+        // O(n)
+
+        unordered_map<int , int> frequency;
+
         int maxLen = 0;
 
-        for(int right = 0; right < nums.size(); right++){
-            while(nums[right] - nums[left] > 1){
-                ++left;
-            }
+        for(int i = 0; i < nums.size(); i++){
+            frequency[nums[i]]++;
+        }
 
-            if(nums[right] - nums[left] == 1){
-                maxLen = max(maxLen , right - left + 1);
+        for(auto& [n, f] : frequency){
+            if(frequency.count(n + 1)){
+                int len = f + frequency[n + 1];
+                maxLen = max(maxLen, len);
             }
         }
 
