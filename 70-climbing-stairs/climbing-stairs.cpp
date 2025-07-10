9ce8efcce0cbf9a9dfe9e77d5ad28b4fbdc7aba6
ananya03 <aggarwalananya03@gmail.com>
2025-07-10 12:33:49 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (87.57%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        int p2 = 1;
+        int p1 = 1;
+
+        for(int i = 2; i <= n; i++){
+            int curr = p1 + p2;
+            p2 = p1;
+            p1 = curr;
+        }
+
+        return p1;
+    }
+};
