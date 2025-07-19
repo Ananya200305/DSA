cb82183ce53cc065a5b6e8ccc286de122e2a7e1f
ananya03 <aggarwalananya03@gmail.com>
2025-07-19 18:40:09 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (8.78%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    // RECURSSION
+    // int helper(int i, int j){
+    //     if(i == 0 && j == 0) return 1;
+    //     if(i < 0 || j < 0) return 0;
+
+    //     return helper(i-1, j) + helper(i , j-1);
+    // }
+
+    int helper(int i, int j, vector<vector<int>> &dp){
+        if(i == 0 && j == 0) return 1;
+        if(i < 0 || j < 0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        return dp[i][j] = helper(i-1, j, dp) + helper(i , j-1, dp);
+    }
+    int uniquePaths(int m, int n) {
+
+        //RECURSSION
+        //return helper(m-1, n-1);
+
+        // MEMOIZATION
+        vector<vector<int>>dp(m, vector<int>(n,-1));
+        return helper(m-1, n-1, dp);
+    }
+};
