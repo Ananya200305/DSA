818ab02a00c36161d61ab6736dd4c8005bf1791b
ananya03 <aggarwalananya03@gmail.com>
2025-07-22 07:07:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (81.21%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> result;
-        result.push_back({});
-        for(int i = 0; i < nums.size(); i++){
-            int size = result.size();
-            for(int j = 0; j < size; j++){
-                vector<int> temp = result[j];
-                temp.push_back(nums[i]);
-                result.push_back(temp);
-            }
+    void helper(vector<int>& nums, int index, vector<int>& current, vector<vector<int>>& result){
+        result.push_back(current);
+
+        for(int i = index; i < nums.size(); i++){
+            current.push_back(nums[i]);
+            helper(nums,i+1,current,result);
+            current.pop_back();
         }
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        // vector<vector<int>> result;
+        // result.push_back({});
+        // for(int i = 0; i < nums.size(); i++){
+        //     int size = result.size();
+        //     for(int j = 0; j < size; j++){
+        //         vector<int> temp = result[j];
+        //         temp.push_back(nums[i]);
+        //         result.push_back(temp);
+        //     }
+        // }
+        // return result;
+
+        vector<vector<int>> result;
+        vector<int> current;
+        helper(nums, 0, current, result);
         return result;
     }
 };
