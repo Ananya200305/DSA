2ceed4554522a29458beac6898951db7e21ee7a8
ananya03 <aggarwalananya03@gmail.com>
2025-08-19 10:50:18 +0530

    Time: 7 ms (60.30%) | Memory: 16.1 MB (48.36%) - LeetSync

---
+++ 139-word-break/word-break.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        vector<bool>dp(s.size()+1, 0);
+        dp[0] = true;
+        unordered_set<string> sets(wordDict.begin(), wordDict.end());
+        for(int i = 1; i <= s.size(); i++){
+            for(int j = 0; j < i; j++){
+                if(dp[j] && sets.count(s.substr(j, i-j))){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[s.size()];
+    }
+};
