fb93c1235c53e80c4f5c5c3b5f63cfc52f47f60a
ananya03 <aggarwalananya03@gmail.com>
2025-08-22 22:56:40 +0530

    Time: 80 ms (64.78%) | Memory: 88.8 MB (65.00%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,73 +1,21 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        // if(nums.size() == 0){
-        //     return 0;
-        // }
-
-        // sort(nums.begin(), nums.end());
-
-        // int count = 1;
-        // int longest = 1;
-
-        // for(int i = 0; i < nums.size() - 1; i++){
-        //     if(nums[i] == nums[i+1]){
-        //         continue;
-        //     }
-        //     if(nums[i] + 1 == nums[i+1]){
-        //         count++;
-        //     }else{
-        //         longest = max(longest,count);
-        //         count = 1;
-        //     }
-        // }
-        // return max(longest,count);
-
-        // unordered_map<int, bool> map;
-
-        // for(int i = 0; i < nums.size(); i++){
-        //     map[nums[i]] = true;
-        // }
-
-        // for(int i = 0; i < nums.size(); i++){
-        //     if(map.count(nums[i] - 1) > 0){
-        //         map[nums[i]] = false;
-        //     }
-        // }
-
-        // int maxLen = 0;
-
-        // for(int i = 0; i < nums.size(); i++){
-        //     if(map[nums[i]] == true){
-        //         int j = 0;
-        //         int count = 0;
-        //         while(map.count(nums[i] + j) > 0){
-        //             j++;
-        //             count++;
-        //         }
-
-        //         if(count > maxLen){
-        //             maxLen = count;
-        //         }
-        //     }
-        // }
-
-        // return maxLen;
-
-        unordered_set<int>set(nums.begin(), nums.end());
+        unordered_set<int>s(nums.begin(), nums.end());
 
         int maxLen = 0;
 
-        for(int num : set){
-            if(set.find(num-1) == set.end()){
+        for(int num : s){
+            if(s.find(num - 1) == s.end()){
                 int curr = num;
-                int count = 1;
-                while(set.count(curr+1)){
+                int len = 1;
+
+                while(s.find(curr + 1) != s.end()){
                     curr++;
-                    count++;
+                    len++;
                 }
 
-                maxLen = max(count, maxLen);
+                maxLen = max(maxLen, len);
             }
         }
 
