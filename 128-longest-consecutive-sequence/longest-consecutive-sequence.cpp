6f4a2f1b9619ec8c4d1d56d8c2940aea4caebef8
ananya03 <aggarwalananya03@gmail.com>
2025-08-23 09:36:54 +0530

    Time: 86 ms (44.90%) | Memory: 88.7 MB (64.96%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+        int maxLen = 0;
         unordered_set<int>s(nums.begin(), nums.end());
 
-        int maxLen = 0;
-
         for(int num : s){
-            if(s.find(num - 1) == s.end()){
-                int curr = num;
+            if(s.find(num-1) == s.end()){
                 int len = 1;
+                int curr = num;
 
                 while(s.find(curr + 1) != s.end()){
-                    curr++;
                     len++;
+                    curr++;
                 }
 
                 maxLen = max(maxLen, len);
