de65a335c8f71f790ff16171f950492b38b7df16
ananya03 <aggarwalananya03@gmail.com>
2025-08-25 13:40:43 +0530

    Time: 0 ms (100.00%) | Memory: 22.4 MB (91.64%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.cpp
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
-        int row = mat.size();
-        int col = mat[0].size();
+        int n = mat.size();
+        int m = mat[0].size();
 
-        vector<int> res(row * col);
-        int r = 0, c = 0;
+        vector<int>res(n*m);
 
-        for (int i = 0; i < res.size(); i++) {
-            res[i] = mat[r][c];
+        int row = 0;
+        int col = 0;
 
-            if ((r + c) % 2 == 0) { // moving up
-                if (c == col - 1) { // last column → move down
-                    r++;
-                } else if (r == 0) { // first row → move right
-                    c++;
-                } else { // normal up-right move
-                    r--;
-                    c++;
-                }
-            } else { // moving down
-                if (r == row - 1) { // last row → move right
-                    c++;
-                } else if (c == 0) { // first col → move down
-                    r++;
-                } else { // normal down-left move
-                    r++;
-                    c--;
-                }
+        for(int i = 0; i < res.size(); i++){
+            res[i] = mat[row][col];
+
+            if((row + col) % 2 == 0){
+                if(col == m-1) row++;
+                else if(row == 0) col++;
+                else { row--; col++; }
+            }else{
+                if(row == n-1) col++;
+                else if(col == 0) row++;
+                else { row++; col--; }
             }
         }
 
