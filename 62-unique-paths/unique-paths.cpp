77b9df016b68e296e311b169c944413173256048
ananya03 <aggarwalananya03@gmail.com>
2025-08-26 10:00:42 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (33.95%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,49 +1,22 @@
 class Solution {
 public:
-
-    // RECURSSION
-    // int helper(int i, int j){
+    // int helper(int i , int j){
     //     if(i == 0 && j == 0) return 1;
     //     if(i < 0 || j < 0) return 0;
 
-    //     return helper(i-1, j) + helper(i , j-1);
+    //     return helper(i-1, j) + helper(i, j-1);
     // }
 
+    int helper(int i , int j, vector<vector<int>>& dp){
+        if(i == 0 && j == 0) return 1;
+        if(i < 0 || j < 0) return 0;
 
-    // MEMOIZATION
-    // int helper(int i, int j, vector<vector<int>> &dp){
-    //     if(i == 0 && j == 0) return 1;
-    //     if(i < 0 || j < 0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
 
-    //     if(dp[i][j] != -1) return dp[i][j];
-
-    //     return dp[i][j] = helper(i-1, j, dp) + helper(i , j-1, dp);
-    // }
-
+        return dp[i][j] = helper(i-1, j, dp) + helper(i, j-1,dp);
+    }
     int uniquePaths(int m, int n) {
-
-        //RECURSSION
-        //return helper(m-1, n-1);
-
-        // MEMOIZATION
-        // vector<vector<int>>dp(m, vector<int>(n,-1));
-        // return helper(m-1, n-1, dp);
-
-        // TABULATION
         vector<vector<int>>dp(m, vector<int>(n,-1));
-
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(i == 0 && j == 0){
-                    dp[i][j] = 1;
-                }else{
-                    int up = i > 0 ? dp[i-1][j] : 0;
-                    int left = j > 0 ? dp[i][j-1] : 0;
-                    dp[i][j] = up + left;
-                }
-            }
-        }
-
-        return dp[m-1][n-1];
+        return helper(m-1, n-1, dp);
     }
 };
