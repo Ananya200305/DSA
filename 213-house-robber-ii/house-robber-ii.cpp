b71e9c2c9e28ed837c65547d5daf01848e8b89be
ananya03 <aggarwalananya03@gmail.com>
2025-08-27 22:44:52 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (40.70%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, int start, int end) {
-        int prev1 = nums[start];
-        int prev2 = 0;
-        int currI = 0;
+    int helper(vector<int>& nums, int i, int end, vector<int>& dp){
+        if(i > end) return 0;
 
-        for(int i = start+1; i <= end; i++){
-            int pick = nums[i];
-            if(i > 1){
-                pick = pick + prev2;
-            }
-            int notPick = 0 + prev1;
-            currI = max(pick, notPick);
-            prev2 = prev1;
-            prev1 = currI;
-        }
+        if(dp[i] != -1) return dp[i];
 
-        return prev1;
+        int steal = nums[i] + helper(nums,i+2, end, dp);
+        int skip = helper(nums, i+1, end, dp);
+
+        return dp[i] = max(steal, skip);
     }
-
     int rob(vector<int>& nums) {
         int n = nums.size();
 
-        if(n==0) return 0;
-        if(n==1) return nums[0];
+        if(n == 1) return nums[0];
+        if(n == 2) return max(nums[0], nums[1]);
 
-        int case1 = helper(nums, 0, n-2);
-        int case2 = helper(nums,1,n-1);
+        vector<int>dp1(n,-1);
+        vector<int>dp2(n,-1);
+
+        int case1 = helper(nums,0, n-2, dp1);
+        int case2 = helper(nums,1,n-1, dp2);
 
         return max(case1, case2);
     }
