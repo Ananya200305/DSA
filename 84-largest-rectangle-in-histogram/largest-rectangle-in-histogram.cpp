e2815a3957232349c52d204575b7a3f8259fc94a
ananya03 <aggarwalananya03@gmail.com>
2025-08-30 13:46:20 +0530

    Time: 27 ms (43.87%) | Memory: 81.3 MB (82.39%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,64 +1,16 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        // int n = heights.size();
-        // vector<int>nse(n);
-        // vector<int>pse(n);
-        // stack<int>st;
-
-        // for(int i = 0; i < n; i++){
-        //     while(!st.empty() && heights[i] <= heights[st.top()]){
-        //         st.pop();
-        //     }
-        //     if(st.empty()){
-        //         pse[i] = -1;
-        //     }else{
-        //         pse[i] = st.top();
-        //     }
-        //     st.push(i);
-        // }
-
-        // while(!st.empty()){
-        //     st.pop();
-        // }
-
-        // for(int i = n-1; i >= 0; i--){
-        //     while(!st.empty() && heights[i] <= heights[st.top()]){
-        //         st.pop();
-        //     }
-        //     if(st.empty()){
-        //         nse[i] = n;
-        //     }else{
-        //         nse[i] = st.top();
-        //     }
-        //     st.push(i);
-        // }
-
-        // int maxArea = 0;
-
-        // for(int i = 0; i < nse.size(); i++){
-        //     int width = nse[i] - pse[i] - 1;
-        //     int area = heights[i] * width;
-        //     maxArea = max(maxArea, area);
-        // }
-
-        // return maxArea;
-
-
-
-        // OPTIMIZED 
-
         heights.push_back(0);
         stack<int>st;
         int maxArea = 0;
 
         for(int i = 0 ; i < heights.size(); i++){
-            while(!st.empty() && heights[i] < heights[st.top()]){
-                int height = heights[st.top()];
+            while(!st.empty() && heights[st.top()] > heights[i]){
+                int h = heights[st.top()];
                 st.pop();
-                int width = st.empty() ? i : i - st.top() - 1;
-                int area = height * width ;
-                maxArea = max(maxArea, area);
+                int w = st.empty() ? i : i - st.top() - 1;
+                maxArea = max(maxArea, h*w);
             }
             st.push(i);
         }
