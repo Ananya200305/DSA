f23d16e4c7a8d258b43be02778afe91cc097d44c
ananya03 <aggarwalananya03@gmail.com>
2025-08-30 18:02:27 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (35.94%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
+        stack<char>st;
 
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+        for(int i = 0 ; i < s.size(); i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
                 st.push(s[i]);
-            } 
-            else if (s[i] == ')') {
-                if (st.empty() || st.top() != '(') {
-                    return false;
-                } else {
-                    st.pop();
-                }
-            } 
-            else if (s[i] == '}') {
-                if (st.empty() || st.top() != '{') {
-                    return false;
-                } else {
-                    st.pop();
-                }
-            } 
-            else if (s[i] == ']') {
-                if (st.empty() || st.top() != '[') {
-                    return false;
-                } else {
-                    st.pop();
-                }
+            }else if(!st.empty() && s[i] == ')' && st.top() == '('){
+                st.pop();
+            }else if(!st.empty() && s[i] == '}' && st.top() == '{'){
+                st.pop();
+            }else if(!st.empty() && s[i] == ']' && st.top() == '['){
+                st.pop();
+            }else{
+                return false;
             }
         }
-        return st.empty();  
+
+        return st.empty();
     }
 };
