9ba3c88cc000cfb2b557443ee38b3ac268c9d942
ananya03 <aggarwalananya03@gmail.com>
2025-08-30 19:11:53 +0530

    Time: 4 ms (15.77%) | Memory: 21.9 MB (72.59%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        vector<int>res;
-        stack<int>st;
+        stack<int> st;
+        vector<int> res;
 
         for(int i = 0; i < asteroids.size(); i++){
             if(asteroids[i] > 0){
