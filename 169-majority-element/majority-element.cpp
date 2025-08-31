ef164e3069f1943662174f9184c649df064dd59b
ananya03 <aggarwalananya03@gmail.com>
2025-08-31 07:40:29 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (62.82%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-    //     sort(nums.begin(),nums.end());
-    //     return nums[nums.size()/2];
-    // } O(n logn)
-
-    // O(n) - Boyer - Moore Voting Algo
-
-        int count = 1;
         int candidate = nums[0];
+        int count = 0;
 
-        for(int i = 1; i < nums.size(); i++){
+        for(int i = 0 ; i < nums.size(); i++){
             if(count == 0){
                 candidate = nums[i];
                 count = 1;
-            }else if(candidate == nums[i]){
+            }else if(nums[i] == candidate){
                 count++;
             }else{
                 count--;
             }
         }
+
         return candidate;
     }
 };
