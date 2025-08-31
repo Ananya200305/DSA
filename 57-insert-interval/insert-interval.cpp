810c72418a0aa73021dcc0fcc0cf23202c1238d2
ananya03 <aggarwalananya03@gmail.com>
2025-08-31 07:58:21 +0530

    Time: 3 ms (31.48%) | Memory: 22.3 MB (8.05%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-
         vector<vector<int>> res;
         bool isInserted = false;
 
-        for(int i = 0; i < intervals.size(); i++){
-            vector<int>interval = intervals[i];
+        for(int i = 0 ; i < intervals.size(); i++){
+            vector<int> interval = intervals[i];
+
             if(interval[1] < newInterval[0]){
                 res.push_back(interval);
             }else if(interval[0] > newInterval[1]){
                 if(!isInserted){
                     res.push_back(newInterval);
                     isInserted = true;
-                } 
+                }
                 res.push_back(interval);
             }else{
                 newInterval[0] = min(newInterval[0], interval[0]);
