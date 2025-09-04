f9923436d6f7c93f60689f2e3f3952f3c1a1f324
ananya03 <aggarwalananya03@gmail.com>
2025-09-04 17:04:23 +0530

    Time: 374 ms (40.78%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+Select query_name,
+round(sum(rating/position)/count(*),2) as quality,
+round(sum(case when rating < 3 then 1 else 0 end)/count(*) * 100.00 ,2) as poor_query_percentage
+from Queries
+group by query_name
