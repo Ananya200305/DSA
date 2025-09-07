c9c7c5e661d0041140f8f59c51b052f4f29d50fb
ananya03 <aggarwalananya03@gmail.com>
2025-09-07 20:09:54 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (73.88%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    int helper(vector<vector<int>>& obstacleGrid, int i, int j, vector<vector<int>>&dp){
-        if(i == 0 && j == 0) return 1;
-        if(i < 0 || j < 0) return 0;
+    // int helper(vector<vector<int>>& obstacleGrid, int i, int j, vector<vector<int>>&dp){
+    //     if(i == 0 && j == 0) return 1;
+    //     if(i < 0 || j < 0) return 0;
 
-        int up = 0;
-        int left = 0;
+    //     int up = 0;
+    //     int left = 0;
 
-        if(dp[i][j] != -1) return dp[i][j];
+    //     if(dp[i][j] != -1) return dp[i][j];
 
-        if(i-1 >= 0 && obstacleGrid[i-1][j] != 1){
-            up = helper(obstacleGrid, i-1, j, dp);
-        }
+    //     if(i-1 >= 0 && obstacleGrid[i-1][j] != 1){
+    //         up = helper(obstacleGrid, i-1, j, dp);
+    //     }
 
-        if(j-1 >= 0 && obstacleGrid[i][j-1] != 1){
-            left = helper(obstacleGrid, i, j-1, dp);
-        }
+    //     if(j-1 >= 0 && obstacleGrid[i][j-1] != 1){
+    //         left = helper(obstacleGrid, i, j-1, dp);
+    //     }
 
-        return dp[i][j] = up + left;
-    }
+    //     return dp[i][j] = up + left;
+    // }
 
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int row = obstacleGrid.size();
@@ -28,8 +28,21 @@ public:
             return 0;
         }
 
-        vector<vector<int>> dp(row, vector<int>(col,-1));
+        vector<vector<int>> dp(row, vector<int>(col,0));
 
-        return helper(obstacleGrid, row - 1, col - 1, dp);
+        dp[0][0] = 1;
+
+        for(int i = 0; i < row ; i++){
+            for(int j = 0; j < col ; j++){
+                if(obstacleGrid[i][j] == 1){
+                    dp[i][j] = 0;
+                }else{
+                    if(i > 0) dp[i][j] += dp[i-1][j];
+                    if(j > 0) dp[i][j] += dp[i][j-1];
+                }
+            }
+        }
+
+        return dp[row-1][col-1];
     }
 };
