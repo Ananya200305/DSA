9807d849d2dfba1b089409fa27e341b173eb01e2
ananya03 <aggarwalananya03@gmail.com>
2025-09-10 12:31:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (17.17%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -2,8 +2,17 @@ class Solution {
 public:
     int fib(int n) {
         if(n == 0) return 0;
-        if(n == 1) return 1;
+        if(n == 1) return 1; 
+        
+        vector<int>seq(n+1,-1);
 
-        return fib(n-1) + fib(n-2);
+        seq[0] = 0;
+        seq[1] = 1;
+
+        for(int i = 2; i < seq.size(); i++){
+            seq[i] = seq[i-1]+seq[i-2];
+        }
+
+        return seq[n];
     }
 };
