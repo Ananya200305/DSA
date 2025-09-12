46063bdbfc3d6f78201cf2a674f6c96b86ee4b9e
ananya03 <aggarwalananya03@gmail.com>
2025-09-12 06:25:21 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (91.25%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size()) return false;
-
         vector<int>count(26,0);
 
-        for(int i = 0 ; i < s.size(); i++){
+        if(s.size() != t.size()) return false;
+
+        for(int i = 0; i < s.size(); i++){
             count[s[i] - 'a']++;
             count[t[i] - 'a']--;
         }
 
-        for(int c : count){
-            if(c != 0) return false;
+        for(int i = 0; i < count.size(); i++){
+            if(count[i] > 0) return false;
         }
 
         return true;
