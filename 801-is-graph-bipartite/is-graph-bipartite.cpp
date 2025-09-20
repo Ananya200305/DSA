4b61b0e6edc1c26ec4fe49a4538054bcb4fdb2ee
ananya03 <aggarwalananya03@gmail.com>
2025-09-20 08:22:25 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (54.84%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
-    bool check(int start, vector<vector<int>>& graph, vector<int>& color){
-        queue<int>q;
-        q.push(start);
-        color[start] = 0;
+    //bfs
 
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
+    // bool check(int start, vector<vector<int>>& graph, vector<int>& color){
+    //     queue<int>q;
+    //     q.push(start);
+    //     color[start] = 0;
 
-            for(auto it : graph[node]){
-                if(color[it] == -1){
-                    color[it] = !color[node];
-                    q.push(it);
-                }else if(color[it] == color[node]){
-                    return false;
-                }
+    //     while(!q.empty()){
+    //         int node = q.front();
+    //         q.pop();
+
+    //         for(auto it : graph[node]){
+    //             if(color[it] == -1){
+    //                 color[it] = !color[node];
+    //                 q.push(it);
+    //             }else if(color[it] == color[node]){
+    //                 return false;
+    //             }
+    //         }
+    //     }
+
+    //     return true;
+    // }
+
+    // dfs
+    bool check(int start, int col, vector<vector<int>>& graph, vector<int>& color){
+        color[start] = col;
+
+        for(auto it : graph[start]){
+            if(color[it] == -1){
+                if(!check(it, !col, graph, color)) return false;
+            }else if(color[it] == color[start]){
+                return false;
             }
         }
 
@@ -25,9 +42,17 @@ public:
     bool isBipartite(vector<vector<int>>& graph) {
         vector<int>color(graph.size(), -1);
 
+        // for(int i = 0 ; i < graph.size(); i++){
+        //     if(color[i] == -1){
+        //         if(!check(i, graph, color)){
+        //             return false;
+        //         }
+        //     }
+        // }
+
         for(int i = 0 ; i < graph.size(); i++){
             if(color[i] == -1){
-                if(!check(i, graph, color)){
+                if(!check(i, 0, graph, color)){
                     return false;
                 }
             }
