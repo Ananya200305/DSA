1d80f2cc1a041a6aa5fd8d7b51b3df723965c379
ananya03 <aggarwalananya03@gmail.com>
2025-09-28 14:25:29 +0530

    Time: 34 ms (20.49%) | Memory: 23.8 MB (27.04%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
-    void bfs(vector<vector<char>>& grid, vector<vector<int>>& visited, int i, int j){
+    void bfs(vector<vector<char>>& grid, int row, int col, vector<vector<bool>>& visited){
         int n = grid.size();
         int m = grid[0].size();
-        visited[i][j] = true;
-        queue<pair<int, int>> q;
-        q.push({i,j});
+
+        visited[row][col] = true;
+        
+        queue<pair<int, int>>q;
+        q.push({row,col});
 
         int delRow[] = {-1, 0, 1, 0};
         int delCol[] = {0, 1, 0, -1};
@@ -13,34 +15,34 @@ public:
         while(!q.empty()){
             int r = q.front().first;
             int c = q.front().second;
-            q.pop();
-            
-            for(int i = 0 ; i < 4; i++){
-                int nRow = r + delRow[i];
-                int nCol = c + delCol[i];
 
-                if(nRow >= 0 && nRow < n && nCol >= 0 && nCol < m 
-                && grid[nRow][nCol] == '1' && !visited[nRow][nCol]){
-                    visited[nRow][nCol] = true;
-                    q.push({nRow, nCol});
+            q.pop();
+
+            for(int i = 0 ; i < 4; i++){
+                int nr = r + delRow[i];
+                int nc = c + delCol[i];
+
+                if(nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == '1' && !visited[nr][nc]){
+                    visited[nr][nc] = true;
+                    q.push({nr,nc});
                 }
             }
         }
     }
 
     int numIslands(vector<vector<char>>& grid) {
-        if(grid.empty() || grid[0].empty()) return 0; 
         int row = grid.size();
         int col = grid[0].size();
 
-        vector<vector<int>> visited(row, vector<int>(col,0));
         int count = 0;
+
+        vector<vector<bool>> visited(row, vector<bool>(col, false));
 
         for(int i = 0 ; i < row; i++){
             for(int j = 0 ; j < col; j++){
                 if(grid[i][j] == '1' && visited[i][j] == false){
                     count++;
-                    bfs(grid, visited, i, j);
+                    bfs(grid, i, j, visited);
                 }
             }
         }
