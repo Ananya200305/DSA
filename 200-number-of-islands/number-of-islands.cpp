0cb5f266b5f9c4f4295541edb73d46824432cf1a
ananya03 <aggarwalananya03@gmail.com>
2025-09-29 05:54:26 +0530

    Time: 29 ms (43.94%) | Memory: 23.8 MB (26.95%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    void bfs(vector<vector<char>>& grid, int row, int col, vector<vector<bool>>& visited){
-        int n = grid.size();
-        int m = grid[0].size();
+    void bfs(vector<vector<char>>& grid, int i, int j, vector<vector<bool>>& visited){
+        int row = grid.size();
+        int col = grid[0].size();
 
-        visited[row][col] = true;
-        
-        queue<pair<int, int>>q;
-        q.push({row,col});
+        visited[i][j] = true;
+
+        queue<pair<int, int>> q;
+        q.push({i,j});
 
         int delRow[] = {-1, 0, 1, 0};
         int delCol[] = {0, 1, 0, -1};
@@ -22,14 +22,13 @@ public:
                 int nr = r + delRow[i];
                 int nc = c + delCol[i];
 
-                if(nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == '1' && !visited[nr][nc]){
+                if(nr >= 0 && nr < row && nc >= 0 && nc < col && grid[nr][nc] == '1' && !visited[nr][nc]){
                     visited[nr][nc] = true;
-                    q.push({nr,nc});
+                    q.push({nr, nc});
                 }
             }
         }
     }
-
     int numIslands(vector<vector<char>>& grid) {
         int row = grid.size();
         int col = grid[0].size();
@@ -39,7 +38,7 @@ public:
         vector<vector<bool>> visited(row, vector<bool>(col, false));
 
         for(int i = 0 ; i < row; i++){
-            for(int j = 0 ; j < col; j++){
+            for(int j = 0; j < col; j++){
                 if(grid[i][j] == '1' && visited[i][j] == false){
                     count++;
                     bfs(grid, i, j, visited);
