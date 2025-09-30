129b023d5a84330ad5da3d40ba642ec283e2c6a1
ananya03 <aggarwalananya03@gmail.com>
2025-10-01 02:54:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (67.60%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x < 0 || (x != 0 && x % 10 == 0)){
-            return false;
-        }
+        if(x < 0 || (x != 0 && x % 10 == 0)) return false;
 
         int reverse = 0;
 
@@ -12,6 +10,6 @@ public:
             x = x / 10;
         }
 
-        return x == reverse || x == reverse / 10;
+        return x == reverse || x == reverse/10;
     }
 };
