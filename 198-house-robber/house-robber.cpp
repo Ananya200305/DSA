10612b832f5a07119c2c3a3570ec041a334803f2
ananya03 <aggarwalananya03@gmail.com>
2025-10-17 21:43:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (46.42%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int helper(vector<int>& nums, int i, vector<int>& dp){
-        if(i >= nums.size()){
-            return 0;
-        }
+        if(i >= nums.size()) return 0;
 
         if(dp[i] != -1) return dp[i];
 
-        int steal = nums[i] + helper(nums,i+2,dp);
-        int skip = helper(nums,i+1,dp);
+        int pick = nums[i] + helper(nums, i+2,dp);
+        int unpick = helper(nums, i+1, dp);
 
-        return dp[i] = max(steal, skip);
+        return dp[i] = max(pick, unpick);
     }
-
     int rob(vector<int>& nums) {
-        vector<int>dp(nums.size(), -1);
-        return helper(nums,0, dp);
+        vector<int> dp(nums.size(), -1);
+        return helper(nums, 0, dp);
     }
 };
