b9a1d64f7c87c9fba11709ca9e12cfe5e11f2eab
ananya03 <aggarwalananya03@gmail.com>
2025-10-18 12:25:15 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (53.95%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,17 @@
+class Solution:
+    def helper(self, nums:List[int], i: int, dp:List[int]) -> int:
+        if i >= len(nums):
+            return 0
+
+        if dp[i] != -1:
+            return dp[i]
+        
+        pick = nums[i] + self.helper(nums, i+2, dp)
+        unpick = self.helper(nums, i+1, dp)
+
+        dp[i] = max(pick, unpick)
+
+        return dp[i]
+    def rob(self, nums: List[int]) -> int:
+        dp = [-1] * len(nums)
+        return self.helper(nums, 0, dp)
