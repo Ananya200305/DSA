c0151f8a491145657af2cd0b112cf49d9f3ad75a
ananya03 <aggarwalananya03@gmail.com>
2025-10-22 13:06:03 +0530

    Time: 1 ms (10.84%) | Memory: 17.8 MB (54.11%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,29 +1,17 @@
 class Solution:
-    # def helper(self, nums:List[int], i: int, dp:List[int]) -> int:
-    #     if i >= len(nums):
-    #         return 0
+    def helper(self, nums: List[int], i: int, dp:List[int]) -> int:
+        if i >= len(nums):
+            return 0
 
-    #     if dp[i] != -1:
-    #         return dp[i]
-        
-    #     pick = nums[i] + self.helper(nums, i+2, dp)
-    #     unpick = self.helper(nums, i+1, dp)
+        if dp[i] != -1:
+            return dp[i]
 
-    #     dp[i] = max(pick, unpick)
+        pick = nums[i] + self.helper(nums, i+2, dp)
+        unpick = self.helper(nums, i+1, dp)
 
-    #     return dp[i]
+        dp[i] = max(pick, unpick)
+
+        return dp[i]
     def rob(self, nums: List[int]) -> int:
         dp = [-1] * (len(nums)+1)
-
-        if len(nums) == 1:
-            return nums[0]
-
-        dp[0] = 0
-        dp[1] = nums[0]
-
-        for i in range(2, (len(nums)+1)):
-            pick = nums[i-1] + dp[i-2]
-            unpick = dp[i-1]
-            dp[i] = max(pick, unpick)
-        
-        return dp[len(nums)]
+        return self.helper(nums, 0, dp)
