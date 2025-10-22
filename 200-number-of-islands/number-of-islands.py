a1d9de79f81d236c7fc18bb318bf7a811ed6f067
ananya03 <aggarwalananya03@gmail.com>
2025-10-22 13:57:32 +0530

    Time: 250 ms (51.27%) | Memory: 20 MB (95.40%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,38 +1,36 @@
 class Solution:
-    def bfs (self, grid: List[List[str]], i: int, j: int, visited: List[List[bool]]) -> None:
+    def bfs(self, grid: list[List[str]], i: int, j : int, visited: List[List[bool]]) -> None:
         row = len(grid)
         col = len(grid[0])
 
-        visited[i][j] = True
-
-        queue = deque()
-        queue.append((i,j))
+        q = deque()
+        q.append((i,j))
 
         x = [-1, 0, 1, 0]
         y = [0, 1, 0, -1]
 
-        while queue:
-            r, c = queue.popleft()
+        while q:
+            r,c = q.popleft()
 
             for i in range(4):
                 nr = r + x[i]
                 nc = c + y[i]
 
-                if 0 <= nr < row and 0 <= nc < col and visited[nr][nc] == False and grid[nr][nc] == '1':
+                if 0 <= nr < row and 0 <= nc < col and grid[nr][nc] == "1" and visited[nr][nc] == False:
                     visited[nr][nc] = True
-                    queue.append((nr,nc))
+                    q.append((nr,nc))
 
     def numIslands(self, grid: List[List[str]]) -> int:
         row = len(grid)
         col = len(grid[0])
 
-        count = 0
-
         visited = [[False for _ in range(col)] for _ in range(row)]
+
+        count = 0
 
         for i in range(row):
             for j in range(col):
-                if grid[i][j] == '1' and visited[i][j] == False:
+                if grid[i][j] == "1" and visited[i][j] == False:
                     count += 1
                     self.bfs(grid, i, j, visited)
 
