6ae370cd16558e1f9426292621821af9f6cb6099
ananya03 <aggarwalananya03@gmail.com>
2025-10-29 09:32:11 +0530

    Time: 7 ms (77.36%) | Memory: 33.4 MB (57.18%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -5,25 +5,23 @@ public:
         int col = grid[0].size();
 
         queue<pair<int, int>>q;
-        q.push({i, j});
+        q.push({i,j});
         visited[i][j] = 1;
 
-        int delRow[] = {-1, 0, 1, 0};
-        int delCol[] = {0, 1, 0, -1};
+        int x[] = {-1, 0, 1, 0};
+        int y[] = {0, 1, 0, -1};
 
         while(!q.empty()){
             int r = q.front().first;
             int c = q.front().second;
+
             q.pop();
 
-            for(int k = 0; k < 4; k++){
-                int nr = r + delRow[k];
-                int nc = c + delCol[k];
+            for(int i = 0; i < 4; i++){
+                int nr = r + x[i];
+                int nc = c + y[i];
 
-                if(nr >= 0 && nr < row
-                && nc >= 0 && nc < col
-                && grid[nr][nc] == 1
-                && !visited[nr][nc]){
+                if(nr >= 0 && nr < row && nc >= 0 && nc < col && grid[nr][nc] == 1 && visited[nr][nc] == 0){
                     visited[nr][nc] = 1;
                     q.push({nr,nc});
                 }
@@ -36,21 +34,21 @@ public:
 
         vector<vector<int>> visited(row, vector<int>(col, 0));
 
-        for(int i = 0 ; i < col; i++){
+        for(int i = 0; i < col; i++){
             if(grid[0][i] == 1 && !visited[0][i]) bfs(grid, visited, 0, i);
             if(grid[row-1][i] == 1 && !visited[row-1][i]) bfs(grid, visited, row-1, i);
         }
 
-        for(int j = 0 ; j < row; j++){
-            if(grid[j][0] == 1 && !visited[j][0]) bfs(grid, visited, j, 0);
-            if(grid[j][col-1] == 1 && !visited[j][col-1]) bfs(grid, visited, j, col-1);
+        for(int i = 0; i < row; i++){
+            if(grid[i][0] == 1 && !visited[i][0]) bfs(grid, visited, i, 0);
+            if(grid[i][col-1] == 1 && !visited[i][col-1]) bfs(grid, visited, i, col-1);
         }
 
         int count = 0;
 
-        for(int i = 0 ; i < row; i++){
-            for(int j = 0 ; j < col; j++){
-                if(grid[i][j] == 1 && !visited[i][j]){
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(grid[i][j] == 1 && visited[i][j] == 0){
                     count++;
                 }
             }
