cdc2437b45a08f74d46b07a8aac6becd9eb9b47c
ananya03 <aggarwalananya03@gmail.com>
2025-10-29 06:14:03 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (85.86%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -10,14 +10,14 @@
  * };
  */
 class Solution {
-public: 
-    bool isMirror(TreeNode* n1, TreeNode* n2){
+public:
+    bool check(TreeNode* n1, TreeNode* n2){
         if(n1 == NULL && n2 == NULL) return true;
         if(n1 == NULL || n2 == NULL) return false;
 
-        return n1 -> val == n2 -> val && isMirror(n1 -> left, n2 -> right) && isMirror(n1 -> right, n2 -> left);
+        return (n1 -> val == n2 -> val) && check(n1 -> left, n2 -> right) && check(n1 -> right, n2 -> left);
     }
     bool isSymmetric(TreeNode* root) {
-        return isMirror(root -> left, root -> right);
+        return check(root -> left, root -> right);
     }
 };
