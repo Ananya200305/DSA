268ec9313da069a63797dbe63b0849c0993eac92
ananya03 <aggarwalananya03@gmail.com>
2025-10-30 17:57:31 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (28.20%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,26 +1,50 @@
 class Solution {
 public:
-    int helper(vector<vector<int>>& grid, int i, int j, vector<vector<int>>& dp){
-        int row = grid.size();
-        int col = grid[0].size();
+    // int helper(vector<vector<int>>& grid, int i, int j, vector<vector<int>>& dp){
+    //     int row = grid.size();
+    //     int col = grid[0].size();
 
-        if(i < 0 || j < 0) return 0;
-        if(grid[i][j] == 1) return 0;
-        if(i == 0 && j == 0) return 1;
+    //     if(i < 0 || j < 0) return 0;
+    //     if(grid[i][j] == 1) return 0;
+    //     if(i == 0 && j == 0) return 1;
 
-        if(dp[i][j] != -1) return dp[i][j];
+    //     if(dp[i][j] != -1) return dp[i][j];
 
-        int up = helper(grid, i-1, j, dp);
-        int left = helper(grid, i , j-1, dp);
+    //     int up = helper(grid, i-1, j, dp);
+    //     int left = helper(grid, i , j-1, dp);
 
-        return dp[i][j] = up + left;
-    }
+    //     return dp[i][j] = up + left;
+    // }
+
+
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
         int row = grid.size();
         int col = grid[0].size();
 
-        vector<vector<int>> dp(row, vector<int>(col, -1));
+        if(grid[0][0] == 1 || grid[row-1][col-1] == 1){
+            return 0;
+        }
 
-        return helper(grid, row-1, col-1, dp);
+
+        vector<vector<int>> dp(row, vector<int>(col, 0));
+
+        dp[0][0] = 1;
+
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(grid[i][j] == 1){
+                    dp[i][j] = 0;
+                }else if (i == 0 && j == 0) {
+                    continue;  
+                }else{
+                    int right = j > 0 ? dp[i][j-1] : 0;
+                    int down = i > 0 ? dp[i-1][j] : 0;
+
+                    dp[i][j] = right + down ;
+                }
+            }
+        }
+
+        return dp[row-1][col-1];
     }
 };
