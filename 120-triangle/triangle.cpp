2c6e9acf458c6d787a0aa70129cb2c78a99acc83
ananya03 <aggarwalananya03@gmail.com>
2025-10-30 14:12:33 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (6.32%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -2,39 +2,35 @@ class Solution {
 public:
     // int helper(vector<vector<int>>& triangle, int i, int j, vector<vector<int>>& dp){
     //     int n = triangle.size();
-
     //     if(i == n-1) return triangle[i][j];
 
     //     if(dp[i][j] != -1) return dp[i][j];
 
     //     int down = helper(triangle, i+1, j, dp);
-    //     int downRight = helper(triangle, i+1, j+1, dp);
+    //     int downR = helper(triangle, i+1, j+1, dp);
 
-    //     return dp[i][j] = triangle[i][j] + min(down, downRight);
+    //     return dp[i][j] = triangle[i][j] + min(down, downR);
     // }
 
+
     int minimumTotal(vector<vector<int>>& triangle) {
-        // int n = triangle.size();
-        // vector<vector<int>>dp(n, vector<int>(n, -1));
-        // return helper(triangle, 0 , 0, dp);
-
+        int ans = INT_MAX;
         int n = triangle.size();
-        
-        vector<int>dp(n);
+        vector<vector<int>> dp(n, vector<int>(n, 0));
 
-        for(int i = 0 ; i < n; i++){
-            dp[i] = triangle[n-1][i];
+        for(int i = 0; i < n; i++){
+            dp[n-1][i] = triangle[n-1][i];
         }
 
         for(int i = n-2; i >= 0; i--){
-            for(int j = 0 ; j <= i; j++){
-                int up = triangle[i][j] + dp[j];
-                int upright = triangle[i][j] + dp[j+1];
+            for(int j = 0; j <= i; j++){
+                int down = dp[i+1][j];
+                int downR = dp[i+1][j+1];
 
-                dp[j] = min(up, upright);
+                dp[i][j] = triangle[i][j] + min(down, downR);
             }
         }
-
-        return dp[0];
+    
+        return dp[0][0];
     }
 };
