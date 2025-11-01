ed96afeecd90e6ceb545d5ad79c4e36f79a3bc6a
ananya03 <aggarwalananya03@gmail.com>
2025-11-01 10:13:49 +0530

    Time: 48 ms (60.35%) | Memory: 35.7 MB (15.22%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> two(vector<int>& nums, int target, int left, int right){
-        vector<vector<int>> pairs;
+        vector<vector<int>> pair;
 
         while(left < right){
             int sum = nums[left] + nums[right];
@@ -11,9 +11,9 @@ public:
             }else if(target < sum){
                 right--;
             }else{
-                pairs.push_back({-target, nums[left], nums[right]});
+                pair.push_back({-target, nums[left], nums[right]});
 
-                while(left < right && nums[left] == nums[left+1]) left++;
+                while(left < right && nums[left] == nums[left + 1]) left++;
                 while(left < right && nums[right] == nums[right-1]) right--;
 
                 left++;
@@ -21,15 +21,14 @@ public:
             }
         }
 
-        return pairs;
+        return pair;
     }
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
         if(n < 3) return {};
-
         sort(nums.begin(), nums.end());
 
-        vector<vector<int>> res;
+        vector<vector<int>>res;
 
         for(int i = 0; i < n-2; i++){
             if(i > 0 && nums[i] == nums[i-1]) continue;
@@ -37,7 +36,7 @@ public:
 
             vector<vector<int>> twoSum = two(nums, first, i+1, n-1);
 
-            for(auto p : twoSum){
+            for(auto & p : twoSum){
                 res.push_back(p);
             }
         }
