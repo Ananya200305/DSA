1627ecf860c8c9a70c4a784b08f8208dc7be0656
ananya03 <aggarwalananya03@gmail.com>
2025-11-03 18:52:03 +0530

    Time: 4 ms (78.11%) | Memory: 14.1 MB (78.68%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
-    int binarysearch(vector<int>& res, int target){
-        int l = 0 ;
-        int r = res.size()-1;
+    int binarySearch(vector<int>& nums, int target){
+        int left = 0;
+        int right = nums.size()-1;
 
-        while(l <= r){
-            int mid = l + (r-l)/2;
-            if(res[mid] == target){
+        while(left <= right){
+            int mid = left + (right - left)/2;
+
+            if(target == nums[mid]){
                 return mid;
-            }else if(res[mid] > target){
-                r = mid -1;
-            }else{
-                l = mid + 1;
+            }else if(target < nums[mid]){
+                right = mid - 1;
+            }else if(target > nums[mid]){
+                left = mid + 1;
             }
         }
 
-        return l;
+        return left;
     }
     int lengthOfLIS(vector<int>& nums) {
-        vector<int>res;
+        vector<int>ans;
 
-        for(int n : nums){
-            if(res.empty() || res.back() < n){
-                res.push_back(n);
+        for(int i = 0; i < nums.size(); i++){
+            if(ans.empty() || ans.back() < nums[i]){
+                ans.push_back(nums[i]);
             }else{
-                int idx = binarysearch(res, n);
-                res[idx] = n;
+                int idx = binarySearch(ans, nums[i]);
+                ans[idx] = nums[i];
             }
         }
 
-        return res.size();
+        return ans.size();
     }
 };
