f805fd3c07237ac5c07656941fff54f099c7f0c9
ananya03 <aggarwalananya03@gmail.com>
2025-11-04 08:04:00 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (34.76%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -4,8 +4,11 @@ public:
         int maxReach = 0;
 
         for(int i = 0; i < nums.size(); i++){
-            if(i > maxReach) return false;
-            maxReach = max(maxReach, i+nums[i]);
+            if(i > maxReach){
+                return false;
+            }
+
+            maxReach = max(maxReach, i + nums[i]);
         }
 
         return true;
