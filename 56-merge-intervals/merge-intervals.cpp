bc47cb2ac661052278ecf05c4bf3143d5607e34a
ananya03 <aggarwalananya03@gmail.com>
2025-11-05 11:22:57 +0530

    Time: 3 ms (89.80%) | Memory: 24.9 MB (13.91%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -5,13 +5,14 @@ public:
             return a[0] < b[0];
         });
 
-        vector<int> prev = intervals[0];
+        vector<vector<int>>res;
 
-        vector<vector<int>> res;
+        vector<int> prev = intervals[0];
 
         for(int i = 1; i < intervals.size(); i++){
             vector<int> interval = intervals[i];
-            if(interval[0] <= prev[1]){
+
+            if(prev[1] >= interval[0]){
                 prev[1] = max(interval[1], prev[1]);
             }else{
                 res.push_back(prev);
