3911dd18b49fb7f73f0030d67d00397758fd9737
ananya03 <aggarwalananya03@gmail.com>
2025-11-05 12:38:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (49.64%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -4,35 +4,36 @@ public:
         int top = 0; 
         int bottom = matrix.size()-1;
         int left = 0;
-        int right = matrix[0].size() - 1;
+        int right = matrix[0].size()-1;
 
-        vector<int> ans;
+        vector<int>res;
 
         while(top <= bottom && left <= right){
             for(int i = left; i <= right; i++){
-                ans.push_back(matrix[top][i]);
+                res.push_back(matrix[top][i]);
             }
             top++;
 
             for(int i = top; i <= bottom; i++){
-                ans.push_back(matrix[i][right]);
+                res.push_back(matrix[i][right]);
             }
             right--;
 
             if(top <= bottom){
                 for(int i = right; i >= left; i--){
-                    ans.push_back(matrix[bottom][i]);
+                    res.push_back(matrix[bottom][i]);
                 }
                 bottom--;
             }
+
             if(left <= right){
                 for(int i = bottom; i >= top; i--){
-                    ans.push_back(matrix[i][left]);
+                    res.push_back(matrix[i][left]);
                 }
                 left++;
             }
         }
 
-        return ans;
+        return res;
     }
 };
