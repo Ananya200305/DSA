a1ea84adcca3cb920df1c637158217b7163da096
ananya03 <aggarwalananya03@gmail.com>
2025-11-06 11:08:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (26.26%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x < 0 || (x != 0 && x % 10 == 0)) return false;
+        if(x < 0 || (x > 0 && x % 10 == 0)) return false;
 
-        long long reverse = 0;
         long long copy = x;
+        long long reverse = 0;
 
         while(copy){
             reverse = reverse * 10 + (copy % 10);
             copy = copy / 10;
         }
 
-        cout << reverse << endl;
         return x == reverse;
     }
 };
