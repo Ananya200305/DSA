34f3edbb17b454cc7b82c04aae2534dad1bc2b35
ananya03 <aggarwalananya03@gmail.com>
2026-01-13 21:41:50 +0530

    Time: 1 ms (24.89%) | Memory: 9.4 MB (50.77%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,36 +1,15 @@
 class Solution {
 public:
-    // int helper(int i, int j, vector<vector<int>>& dp){
-    //     if(i == 0 && j == 0) return 1;
-    //     if(i < 0 || j < 0) return 0;
+    int helper(int x, int y, vector<vector<int>>& dp){
+        if(x == 0 && y == 0) return 1;
+        if(x < 0 || y < 0) return 0;
 
-    //     if(dp[i][j] != -1) return dp[i][j];
+        if(dp[x][y] != -1) return dp[x][y];
 
-    //     int up = helper(i-1, j, dp);
-    //     int left = helper(i,j-1, dp);
-
-    //     return dp[i][j] = up+left;
-    // }
-
-    
+        return dp[x][y] = helper(x-1, y, dp) + helper(x, y-1, dp);
+    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m, vector<int>(n, 0));
-        
-        dp[0][0] = 1;
-
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(i == 0 && j == 0){
-                    dp[i][j] = 1;
-                }else{
-                    int right = j > 0 ? dp[i][j-1] : 0;
-                    int down = i > 0 ? dp[i-1][j] : 0;
-
-                    dp[i][j] = right + down;
-                }
-            }
-        }
-
-        return dp[m-1][n-1];
+        vector<vector<int>>dp(m, vector<int>(n,-1));
+        return helper(m-1, n-1, dp);
     }
 };
