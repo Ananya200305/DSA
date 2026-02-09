f51460197986df5c9a4425ce81ca45bd75c2eb74
ananya03 <aggarwalananya03@gmail.com>
2026-02-09 18:52:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Minimum number of Coins/minimum-number-of-coins.cpp
+++ Difficulty: Easy/Minimum number of Coins/minimum-number-of-coins.cpp
@@ -1,13 +1,14 @@
 class Solution {
   public:
     int findMin(int n) {
+        // code here
         int count = 0;
-        vector<int>denomination = {1,2,5,10};
+        vector<int>d = {1,2,5,10};
         
-        for(int i = denomination.size()-1; i >= 0; i--){
-            while(n >= denomination[i]){
-                n = n - denomination[i];
+        for(int i = d.size()-1; i >= 0; i--){
+            while(n >= d[i]){
                 count++;
+                n = n - d[i];
             }
         }
         
