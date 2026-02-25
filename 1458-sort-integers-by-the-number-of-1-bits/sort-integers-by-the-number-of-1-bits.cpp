2d05ed132ef62fba7a6d22bc5985e1218750e6d3
ananya03 <aggarwalananya03@gmail.com>
2026-02-25 22:06:52 +0530

    Time: 2 ms (52.93%) | Memory: 15 MB (16.23%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -1,27 +1,16 @@
 class Solution {
 public:
-    int helper(int n){
-        int count = 0;
-
-        while(n){
-            count += n & 1;
-            n = n >> 1;
-        }
-        
-        return count;
-    }
     vector<int> sortByBits(vector<int>& arr) {
-        vector<pair<int, int>>res;
+        vector<pair<int, int>> res;
 
-        for(int i = 0 ; i < arr.size(); i++){
-            int x = helper(arr[i]);
-            cout << x << endl;
+        for(int i = 0; i < arr.size(); i++){
+            int x = __builtin_popcount(arr[i]);
             res.push_back({x, arr[i]});
         }
 
         sort(res.begin(), res.end());
 
-        vector<int> ans;
+        vector<int>ans;
 
         for(int i = 0 ; i < res.size(); i++){
             ans.push_back(res[i].second);
