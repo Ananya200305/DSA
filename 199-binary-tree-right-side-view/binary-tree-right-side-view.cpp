ea19ebcf72104e7e1dcd858b298e0b3dacdecc6d
ananya03 <aggarwalananya03@gmail.com>
2026-04-20 18:12:32 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (21.53%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -14,12 +14,15 @@ public:
     vector<int> rightSideView(TreeNode* root) {
         vector<int>ans;
         if(root == NULL) return ans;
+
         queue<TreeNode*>q;
+
         q.push(root);
+
         while(!q.empty()){
             int size = q.size();
             vector<int>level;
-            for(int i = 0 ; i < size; i++){
+            for(int i = 0; i < size; i++){
                 TreeNode* node = q.front();
                 q.pop();
 
@@ -27,8 +30,7 @@ public:
 
                 if(node -> left != NULL){
                     q.push(node -> left);
-                } 
-
+                }
                 if(node -> right != NULL){
                     q.push(node -> right);
                 }
