546def7d99e4e9403ccdf67cd44270262eeb61e0
ananya03 <aggarwalananya03@gmail.com>
2026-04-22 03:59:45 +0530

    Time: 549 ms (25.40%) | Memory: 243.4 MB (46.96%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.cpp
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -13,8 +13,9 @@ public:
         }
 
         vector<int>temp;
+
         int left = low;
-        int right = mid + 1;
+        int right = mid+1;
 
         while(left <= mid && right <= high){
             if(nums[left] <= nums[right]){
@@ -32,7 +33,7 @@ public:
             temp.push_back(nums[right++]);
         }
 
-        for(int i = low ; i <= high; i++){
+        for(int i = low; i <= high; i++){
             nums[i] = temp[i-low];
         }
 
@@ -41,9 +42,9 @@ public:
     int mergeSort(vector<int>& nums, int low, int high){
         if(low >= high) return 0;
 
-        int count = 0;
-
         int mid = low + (high - low)/2;
+
+        int count = 0;
 
         count += mergeSort(nums, low, mid);
         count += mergeSort(nums, mid+1, high);
