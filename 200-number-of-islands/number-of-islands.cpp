6e869f8a1b274a8125267bd9a764dcdebc22a641
ananya03 <aggarwalananya03@gmail.com>
2026-09-25 19:15:50 +0530

    Time: 23 ms (83.23%) | Memory: 23.7 MB (23.67%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    void bfs(vector<vector<char>>& grid, int i, int j, vector<vector<bool>>& visited){
+    void bfs(vector<vector<bool>>& visited, int x, int y, vector<vector<char>>& grid){
         int row = grid.size();
         int col = grid[0].size();
 
-        visited[i][j] = true;
+        queue<pair<int, int>>q;
+        q.push({x,y});
 
-        queue<pair<int, int>> q;
-        q.push({i,j});
-
-        int delRow[] = {-1, 0, 1, 0};
-        int delCol[] = {0, 1, 0, -1};
+        int i[] = {-1, 0, 1, 0};
+        int j[] = {0, 1, 0, -1};
 
         while(!q.empty()){
             int r = q.front().first;
@@ -18,30 +16,31 @@ public:
 
             q.pop();
 
-            for(int i = 0 ; i < 4; i++){
-                int nr = r + delRow[i];
-                int nc = c + delCol[i];
+            for(int z = 0; z < 4; z++){
+                int nr = r + i[z];
+                int nc = c + j[z];
 
-                if(nr >= 0 && nr < row && nc >= 0 && nc < col && grid[nr][nc] == '1' && !visited[nr][nc]){
+                if(nr >= 0 && nr < row && nc >= 0 && nc < col && grid[nr][nc] == '1' && visited[nr][nc] == false){
                     visited[nr][nc] = true;
                     q.push({nr, nc});
                 }
             }
         }
     }
+
     int numIslands(vector<vector<char>>& grid) {
         int row = grid.size();
         int col = grid[0].size();
 
-        int count = 0;
-
         vector<vector<bool>> visited(row, vector<bool>(col, false));
 
-        for(int i = 0 ; i < row; i++){
+        int count = 0;
+
+        for(int i = 0; i < row; i++){
             for(int j = 0; j < col; j++){
                 if(grid[i][j] == '1' && visited[i][j] == false){
                     count++;
-                    bfs(grid, i, j, visited);
+                    bfs(visited, i, j, grid);
                 }
             }
         }
